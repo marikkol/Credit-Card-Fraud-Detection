9bb2dd3a17fac4db862966e3682ceada2b80d8db
Marik Koltman <marikoltman2@gmail.com>
2022-06-21 10:46:37 +0300

    mini exp

--- preprossesing.py
+++ preprossesing.py
@@ -8,11 +8,12 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.model_selection import StratifiedKFold
 from sklearn.feature_selection import RFECV
 from sklearn.svm import SVC
 from sklearn.pipeline import Pipeline
 from imblearn.combine import SMOTEENN
-
+from imblearn.under_sampling import EditedNearestNeighbours, RandomUnderSampler
 
 
 
@@ -45,7 +46,6 @@ test_df = test_df.reindex(columns=train_df.columns.tolist().remove('isFraud'))  
 
 # Missing Values
 
-
 missing_val_df = train_df.isnull().sum().to_frame()
 missing_val_df = missing_val_df.apply(lambda x: x/len(train_df)*100, axis=1)\
     .rename(columns={0: 'Missing Ratio'}).sort_values(by=['Missing Ratio'])
@@ -68,11 +68,10 @@ print(f"Count total NaN in a DataFrame : {train_df.isnull().sum().sum()}")
 # mode for categorical features
 cat_cols = list(set(train_df.columns)-set(num_cols))
 train_df[cat_cols] = train_df[cat_cols].fillna(train_df[cat_cols].mode().iloc[0])
-print(f"Count total NaN in a DataFrame : {train_df.isnull().sum().sum()}"
+print(f"Count total NaN in a DataFrame : {train_df.isnull().sum().sum()}")
 
 
 # Encoding Catigorical Features
-
 
 nunique = train_df[cat_cols].nunique()
 print(nunique)   # 8 of them contained only two levels of cardinality (e.g., attributes with false and true)
@@ -90,9 +89,7 @@ df = pd.get_dummies(train_df, columns=rets_cat_features)
 print(df)
 
 
-
 # Feature Scaling
-
 
 # MinMaxScaler scaling:  x' = (x - min(x))/max(x) - min(x)
 num_cols.remove('isFraud')
@@ -101,12 +98,9 @@ df[num_cols] = min_max_scaler.fit_transform(df[num_cols])
 print(df)
 
 
-
 # Feature Selection
 
-
 # Filter method - hybrid feature selection for numerical features - correlation matrix
-
 corrmat = df[num_cols].corr().abs()
 f, ax = plt.subplots(figsize=(12, 9))
 sns.heatmap(corrmat, vmax=.8, square=True)
@@ -125,26 +119,37 @@ y_train, y_test = df_train.pop('isFraud'), df_test.pop('isFraud')
 X_train, X_test = df_train, df_test
 
 # Wrapper method - SVM-RFE
-
-rand_col_select = random.sample(list(X_train.columns), 30)
+rand_col_select = random.sample(list(X_train.columns), 15)
 X_train, X_test = X_train[rand_col_select], X_test[rand_col_select]
 
 svc = SVC(kernel="linear")
-rfecv = RFECV(estimator=svc, step=0.2, min_features_to_select=10, verbose=1)
-pipeline = Pipeline(steps=[('s',rfecv),('m',svc)])
-cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=3, random_state=1)
-n_scores = cross_val_score(pipeline, X_train, y_train, scoring='accuracy', cv=cv, error_score='raise')
-print('Accuracy: %.3f (%.3f)' % (mean(n_scores), std(n_scores)))
+min_features_to_select = 10
+rfecv = RFECV(estimator=svc, step=0.2, cv=StratifiedKFold(2), min_features_to_select=min_features_to_select, verbose=1)
+rfecv = rfecv.fit(X_train, y_train)
+print('Optimal number of features :', rfecv.n_features_)
+# pipeline = Pipeline(steps=[('s',rfecv),('m',svc)])
+# cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=1, random_state=1)
+# n_scores = cross_val_score(pipeline, X_train, y_train, scoring='accuracy', cv=5, error_score='raise')
+# print('Accuracy: %.3f (%.3f)' % (np.mean(n_scores), np.std(n_scores)))
 final_features = X_train.columns[rfecv.support_]
 print(f"Selected features: {final_features}")
-n_features = rfecv.n_features_
-print(f"Optimal number of features {n_features}")
 
 # Plot showing the Cross Validation score
+num_feat_list = [len(rand_col_select)]   # list of Number of features selected for plotting
+while (num_feat_list[-1]-num_feat_list[-1]*0.2) >= 10:
+    num_feat_list.append(round(num_feat_list[-1]-num_feat_list[-1]*0.2))
+if num_feat_list[-1] != min_features_to_select:
+    num_feat_list.append(min_features_to_select)
+
+print(num_feat_list[::-1])
+print(rfecv.grid_scores_)
+print(type(rfecv.grid_scores_))
+
 plt.figure()
 plt.xlabel("Number of features selected")
-plt.ylabel("Cross validation score (nb of correct classifications)")
-plt.plot(range(1, len(rfecv.grid_scores_) + 1), rfecv.grid_scores_)
+plt.ylabel("Cross validation score (accuracy)")
+plt.plot(num_feat_list[::-1], np.mean(rfecv.grid_scores_, axis=1))   # taking the mean of cv scores
+
 plt.show()
 
 X_train_rfe = X_train[final_features]
@@ -153,20 +158,137 @@ print(X_train_rfe)
 print(X_test_rfe)
 
 
-
 # Data Resampling
 
-
 # The classes are heavily skewed
-print('No Frauds', round(y_train.value_counts()[0]/len(y_train) * 100,2), '% of the dataset')
-print('Frauds', round(y_train.value_counts()[1]/len(y_train) * 100,2), '% of the dataset')
 colors = ["#0101DF", "#DF0101"]
-sns.countplot('isFraud', data=y_train, palette=colors)
-plt.title('Class Distributions \n (0: No Fraud || 1: Fraud)', fontsize=14)
-plt.show()
+def plot_dist(y_data):
+    print('No Frauds', round(y_data.value_counts()[0]/len(y_data) * 100,2), '% of the dataset')
+    print('Frauds', round(y_data.value_counts()[1]/len(y_data) * 100,2), '% of the dataset')
+    print(y_data)
+    ser_count_n = y_data.value_counts(normalize=True)
+    print(ser_count_n)
+    ser_count_n = ser_count_n.mul(100)
+    print(ser_count_n)
+    df_count_n = ser_count_n.rename('percent').reset_index().rename(columns={"index": "Is Fraud"})
+    print(df_count_n)
+    g = sns.catplot(x='Is Fraud',y='percent',kind='bar',data=df_count_n)
+    plt.title('Class Distributions \n (0: No Fraud || 1: Fraud)', fontsize=14)
+    g.ax.set_ylim(0,100)
+    for p in g.ax.patches:
+        txt = str(p.get_height().round(2)) + '%'
+        txt_x = p.get_x()
+        txt_y = p.get_height()
+        g.ax.text(txt_x,txt_y,txt)
+    plt.tight_layout()
+    plt.show()
 
+    ser_count = y_data.value_counts()
+    print(ser_count)
+    df_count = ser_count.rename('examples').reset_index().rename(columns={"index": "Is Fraud"})
+    print(df_count)
+    g = sns.catplot(x='Is Fraud',y='examples',kind='bar',data=df_count)
+    plt.title('Class Distributions \n (0: No Fraud || 1: Fraud)', fontsize=14)
+    for p in g.ax.patches:
+        txt = str(int(p.get_height().round()))
+        txt_x = p.get_x()
+        txt_y = p.get_height()
+        g.ax.text(txt_x,txt_y,txt)
+    plt.tight_layout()
+    plt.show()
+
+plot_dist(y_train)
+
+
+# sns.countplot(y=y_train, palette=colors)
+# plt.title('Class Distributions \n (0: No Fraud || 1: Fraud)', fontsize=14)
+# plt.show()
+
+# Resample the dataset using SMOTE-ENN
+sme = SMOTEENN(enn=EditedNearestNeighbours(sampling_strategy='all'), random_state=42)
 X_res, y_res = sme.fit_resample(X_train_rfe, y_train)
 
+# The classes dist after SMOTE-ENN
+plot_dist(y_res)
+
+# print('No Frauds', round(y_res.value_counts()[0]/len(y_res) * 100,2), '% of the dataset')
+# print('Frauds', round(y_res.value_counts()[1]/len(y_res) * 100,2), '% of the dataset')
+# ser_count_n = y_res.value_counts(normalize=True)
+# print(ser_count_n)
+# ser_count_n = ser_count_n.mul(100)
+# print(ser_count_n)
+# df_count_n = ser_count_n.rename('percent').reset_index().rename(columns={"index": "Is Fraud"})
+# print(df_count_n)
+# g = sns.catplot(x='Is Fraud',y='percent',kind='bar',data=df_count_n)
+# plt.title('Class Distributions SMOTE-ENN \n (0: No Fraud || 1: Fraud)', fontsize=14)
+# g.ax.set_ylim(0,100)
+# for p in g.ax.patches:
+#     txt = str(p.get_height().round(2)) + '%'
+#     txt_x = p.get_x()
+#     txt_y = p.get_height()
+#     g.ax.text(txt_x,txt_y,txt)
+# plt.tight_layout()
+# plt.show()
+#
+# ser_count = y_res.value_counts()
+# print(ser_count)
+# df_count = ser_count.rename('examples').reset_index().rename(columns={"index": "Is Fraud"})
+# print(df_count)
+# g = sns.catplot(x='Is Fraud',y='examples',kind='bar',data=df_count)
+# plt.title('Class Distributions SMOTE-ENN \n (0: No Fraud || 1: Fraud)', fontsize=14)
+# for p in g.ax.patches:
+#     txt = str(p.get_height().round())
+#     txt_x = p.get_x()
+#     txt_y = p.get_height()
+#     g.ax.text(txt_x,txt_y,txt)
+# plt.tight_layout()
+# plt.show()
+
+# Under-sample the majority class
+rus = RandomUnderSampler(random_state=42)
+rus.fit(X_res, y_res)
+X_res, y_res = rus.fit_resample(X_res, y_res)
 
 
+# The classes dist after Under-sample
+plot_dist(y_res)
+
+
+# print('No Frauds', round(y_res.value_counts()[0]/len(y_res) * 100,2), '% of the dataset')
+# print('Frauds', round(y_res.value_counts()[1]/len(y_res) * 100,2), '% of the dataset')
+# ser_count_n = y_res.value_counts(normalize=True)
+# print(ser_count_n)
+# ser_count_n = ser_count_n.mul(100)
+# print(ser_count_n)
+# df_count_n = ser_count_n.rename('percent').reset_index().rename(columns={"index": "Is Fraud"})
+# print(df_count_n)
+# g = sns.catplot(x='Is Fraud',y='percent',kind='bar',data=df_count_n)
+# plt.title('Class Distributions SMOTE-ENN \n (0: No Fraud || 1: Fraud)', fontsize=14)
+# g.ax.set_ylim(0,100)
+# for p in g.ax.patches:
+#     txt = str(p.get_height().round(2)) + '%'
+#     txt_x = p.get_x()
+#     txt_y = p.get_height()
+#     g.ax.text(txt_x,txt_y,txt)
+# plt.tight_layout()
+# plt.show()
+#
+# ser_count = y_res.value_counts()
+# print(ser_count)
+# df_count = ser_count.rename('examples').reset_index().rename(columns={"index": "Is Fraud"})
+# print(df_count)
+# g = sns.catplot(x='Is Fraud',y='examples',kind='bar',data=df_count)
+# plt.title('Class Distributions SMOTE-ENN \n (0: No Fraud || 1: Fraud)', fontsize=14)
+# for p in g.ax.patches:
+#     txt = str(p.get_height().round())
+#     txt_x = p.get_x()
+#     txt_y = p.get_height()
+#     g.ax.text(txt_x,txt_y,txt)
+# plt.tight_layout()
+# plt.show()
+
+# save arrays to one file in compressed format
+X_train = X_res.to_numpy()
+y_train = y_res.to_numpy()
+np.savez_compressed('preprossesing_data.npz', X_train, y_train, X_test, y_test)
 
